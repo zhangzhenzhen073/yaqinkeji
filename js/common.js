46f8254e43e7549b88fe3faf7e343c3e1c2a273d
zhangzhenzhen073 <usernam736601900@qq.com>
2017-12-08 16:42:42 +0800

    tj

--- js/common.js
+++ js/common.js
@@ -1,6 +1,15 @@
 /**
  * Created by Administrator on 2017/11/24.
  */
+/*rem 动态计算html的字体 大小*/
+function refreshRem(){
+    var desW = 1920 ;
+    var winW = document.documentElement.clientWidth;
+    document.documentElement.style.fontSize = winW / desW *100 +'px';
+}
+refreshRem();
+window.addEventListener('resize',refreshRem);
+
 
 $(document).ready(function() {
     $('#fullpage').fullpage({
